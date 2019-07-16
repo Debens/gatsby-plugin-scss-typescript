0a90a97a895bffa196815d0e1b14b494da6df530
Debens <a.debens@gmail.com>
2019-07-16 14:32:42 +0100

    chore: using yml rc config allround

--- husky.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    hooks: {
-        'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
-        'pre-commit': 'circleci config validate && yarn lint:staged',
-        'pre-push': 'yarn test:coverage',
-    },
-    skipCI: true,
-};
