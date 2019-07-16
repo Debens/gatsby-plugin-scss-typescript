11d5cad61bd7c398410b5e77098126695c360d19
Debens <a.debens@gmail.com>
2019-07-16 11:16:47 +0100

    chore: removing duplicate config files

--- husky.config.js
+++ husky.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    skipCI: true,
     hooks: {
+        'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
         'pre-commit': 'circleci config validate && yarn lint:staged',
         'pre-push': 'yarn test:coverage',
-        'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
     },
+    skipCI: true,
 };
