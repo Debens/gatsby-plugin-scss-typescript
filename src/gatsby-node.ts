cd99f191bef380918cfecbf8491d8ade55a8b6bf
Nick Wehner <nwehner@users.noreply.github.com>
2021-04-21 11:34:07 -0700

    Update deps and match local code

--- src/gatsby-node.ts
+++ src/gatsby-node.ts
@@ -5,96 +5,93 @@ import { RuleSetUseItem } from 'webpack';
 
 type ObjectRule = Exclude<RuleSetUseItem, string | Function>;
 
-export const onCreateWebpackConfig = ({ stage, plugins, rules, actions }, options) => {
-    const { setWebpackConfig } = actions;
-    const isProduction = !stage.includes('develop');
+// While `rules` isn't used here, we must have it around for tests
+export const onCreateWebpackConfig = ({ stage, plugins, actions, rules }, options) => {
+	const { setWebpackConfig } = actions;
+	const isProduction = !stage.includes('develop');
 
-    const {
-        cssMinifyOptions,
-        cssExtractOptions,
-        cssLoaderOptions,
-        sassLoaderOptions,
-        declarationOptions,
-    } = options;
+	const {
+		cssMinifyOptions,
+		cssExtractOptions,
+		cssLoaderOptions,
+		sassLoaderOptions,
+		declarationOptions,
+	} = options;
 
-    const typeLoader: ObjectRule = {
-        loader: '@teamsupercell/typings-for-css-modules-loader',
-        options: declarationOptions,
-    };
+	const typeLoader: ObjectRule = {
+		loader: "dts-css-modules-loader",
+		options: {
+			namedExport: true,
+			...declarationOptions
+		}
+	};
+	const styleLoader: ObjectRule = {
+		loader: "style-loader",
+		options: {
+			esModule: false,
+		},
+	};
+	const cssLoader: ObjectRule = {
+		loader: "css-loader",
+		options: {
+			...cssLoaderOptions,
+			modules: {
+				exportLocalsConvention: "camelCaseOnly",
+				localIdentName: "[local]"
+			}
+		}
+	};
+	const sassLoader: ObjectRule = {
+		loader: "sass-loader",
+		options: {
+			sourceMap: !isProduction,
+			...sassLoaderOptions
+		},
+	};
 
-    const useCss: ObjectRule[] = rules.css(cssLoaderOptions).use;
-    const useCssModules: ObjectRule[] = rules.cssModules(cssLoaderOptions).use;
+	const sassLoaders: ObjectRule[] = [
+		styleLoader,
+		cssLoader,
+		sassLoader
+	];
 
-    const sassLoaders: ObjectRule[] = [
-        { loader: 'resolve-url-loader' },
-        {
-            loader: 'sass-loader',
-            options: {
-                sourceMap: !isProduction,
-                ...sassLoaderOptions,
-            },
-        },
-    ];
+	const typeLoaders: ObjectRule[] = [
+		styleLoader,
+		typeLoader,
+		cssLoader,
+		sassLoader
+	];
 
-    const isCssLoader = ({ loader }: ObjectRule) =>
-        !!['/css-loader/', '\\css-loader\\'].find(matcher => loader.includes(matcher));
-
-    /**
-     * https://github.com/TeamSupercell/typings-for-css-modules-loader/blob/master/src/index.js#L64-L66
-     *
-     * TeamSupercell use the locals property in the named exports.
-     *
-     * Sadly this means we have to disable the tree shaking when generating the typings to allow the typings generation.
-     * This wil have no effect on the actual tree-shaking of course
-     */
-    const toDisabledTreeShaking = loader => ({
-        ...loader,
-        options: {
-            ...loader.options,
-            modules: {
-                ...loader.options.modules,
-                namedExport: false,
-            },
-        },
-    });
-
-    const css = useCss.concat(sassLoaders);
-    const cssModules = useCssModules
-        .flatMap(loader =>
-            isCssLoader(loader) ? [typeLoader, toDisabledTreeShaking(loader)] : loader,
-        )
-        .concat(sassLoaders);
-
-    switch (stage) {
-        case 'develop':
-        case 'build-javascript':
-        case 'build-html':
-        case 'develop-html': {
-            setWebpackConfig({
-                module: {
-                    rules: [
-                        {
-                            oneOf: [
-                                {
-                                    test: /\.module\.s(a|c)ss$/,
-                                    use: cssModules,
-                                },
-                                {
-                                    test: /\.s(a|c)ss$/,
-                                    use: css,
-                                },
-                            ],
-                        },
-                    ],
-                },
-                optimization: {
-                    minimizer: [plugins.minifyCss(cssMinifyOptions)],
-                },
-                plugins: [
-                    plugins.extractText(cssExtractOptions),
-                    plugins.ignore(/css\.d\.ts$/),
-                ],
-            });
-        }
-    }
+	switch (stage) {
+		case 'develop':
+		case 'build-javascript':
+		case 'build-html':
+		case 'develop-html': {
+			setWebpackConfig({
+				module: {
+					rules: [
+						{
+							oneOf: [
+								{
+									test: /\.module\.s(a|c)ss$/,
+									use: typeLoaders,
+								},
+								{
+									test: /\.s(a|c)ss$/,
+									use: sassLoaders,
+								},
+							],
+						},
+					],
+				},
+				optimization: {
+					minimizer: [plugins.minifyCss(cssMinifyOptions)],
+				},
+				plugins: [
+					plugins.extractText(cssExtractOptions),
+					plugins.ignore(/css\.d\.ts$/),
+				],
+			});
+		}
+	}
 };
