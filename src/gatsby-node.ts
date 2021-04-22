4cc6b5b920435a14feb871212eee548aeabb0287
Nick Wehner <nwehner@users.noreply.github.com>
2021-04-22 08:50:37 -0700

    Builds CSS and types correctly, but possible error message that styles.module.scss is not a module

--- src/gatsby-node.ts
+++ src/gatsby-node.ts
@@ -6,7 +6,7 @@ import { RuleSetUseItem } from 'webpack';
 type ObjectRule = Exclude<RuleSetUseItem, string | Function>;
 
 // While `rules` isn't used here, we must have it around for tests
-export const onCreateWebpackConfig = ({ stage, plugins, actions, loaders, rules }, options) => {
+export const onCreateWebpackConfig = ({ stage, plugins, actions, rules }, options) => {
 	const { setWebpackConfig } = actions;
 	const isProduction = !stage.includes('develop');
 
@@ -18,6 +18,9 @@ export const onCreateWebpackConfig = ({ stage, plugins, actions, loaders, rules 
 		declarationOptions,
 	} = options;
 
+	const useCss: ObjectRule[] = rules.css(cssLoaderOptions).use;
+	const useCssModules: ObjectRule[] = rules.cssModules(cssLoaderOptions).use;
+
 	const typeLoader: ObjectRule = {
 		loader: "dts-css-modules-loader",
 		options: {
@@ -25,16 +28,6 @@ export const onCreateWebpackConfig = ({ stage, plugins, actions, loaders, rules 
 			...declarationOptions
 		}
 	};
-	const styleLoader: ObjectRule = loaders.style({
-		esModule: false,
-	});
-	const cssLoader: ObjectRule = loaders.css({
-		...cssLoaderOptions,
-		modules: {
-			exportLocalsConvention: "camelCaseOnly",
-			localIdentName: "[local]"
-		}
-	});
 	const sassLoader: ObjectRule = {
 		loader: "sass-loader",
 		options: {
@@ -43,49 +36,38 @@ export const onCreateWebpackConfig = ({ stage, plugins, actions, loaders, rules 
 		},
 	};
 
-	const sassLoaders: ObjectRule[] = [
-		styleLoader,
-		cssLoader,
+	const sassLoaders: ObjectRule[] = useCss.concat([
 		sassLoader
-	];
+	]);
 
-	const typeLoaders: ObjectRule[] = [
-		styleLoader,
+	const typeLoaders: ObjectRule[] = useCssModules.concat([
 		typeLoader,
-		cssLoader,
 		sassLoader
-	];
+	]);
 
-	switch (stage) {
-		case 'develop':
-		case 'build-javascript':
-		case 'build-html':
-		case 'develop-html': {
-			setWebpackConfig({
-				module: {
-					rules: [
+	setWebpackConfig({
+		module: {
+			rules: [
+				{
+					oneOf: [
 						{
-							oneOf: [
-								{
-									test: /\.module\.s(a|c)ss$/,
-									use: typeLoaders,
-								},
-								{
-									test: /\.s(a|c)ss$/,
-									use: sassLoaders,
-								},
-							],
+							test: /\.module\.s(a|c)ss$/,
+							use: typeLoaders,
+						},
+						{
+							test: /\.s(a|c)ss$/,
+							use: sassLoaders,
 						},
 					],
 				},
-				optimization: {
-					minimizer: [plugins.minifyCss(cssMinifyOptions)],
-				},
-				plugins: [
-					plugins.extractText(cssExtractOptions),
-					plugins.ignore(/css\.d\.ts$/),
-				],
-			});
-		}
-	}
+			],
+		},
+		optimization: {
+			minimizer: [plugins.minifyCss(cssMinifyOptions)],
+		},
+		plugins: [
+			plugins.extractText(cssExtractOptions),
+			plugins.ignore(/css\.d\.ts$/),
+		],
+	});
 };
